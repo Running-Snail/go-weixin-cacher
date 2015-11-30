fd07c4fcd179f0b015b2f7f6f3e836f8fd824322
Ji Zihao <zhao11fs@gmail.com>
2015-11-30 21:49:13 +0800

    change to store string instead of struct

--- main.go
+++ main.go
@@ -35,10 +35,7 @@ func cacheAccessToken(accessTokenCH chan string) {
         log.Println("saving access token")
         err = mc.Set(&memcache.Item{
             Key: accessTokenKey,
-            Object: map[string]interface{} {
-                "access_token": accessTokenResp.AccessToken,
-                "expires_in": accessTokenResp.ExpiresIn,
-            },
+            Value: []byte(accessTokenResp.AccessToken),
         })
         if err != nil {
             log.Println("save access token error")
@@ -75,10 +72,7 @@ func cacheJSSDKTicket(accessTokenCH chan string) {
         log.Println("saving jssdk ticket")
         err = mc.Set(&memcache.Item{
             Key: jssdkTicketKey,
-            Object: map[string]interface{} {
-                "ticket": jssdkTicketResponse.Ticket,
-                "expires_in": jssdkTicketResponse.ExpiresIn,
-            },
+            Value: []byte(jssdkTicketResponse.Ticket),
         })
         if err != nil {
             log.Println("save jssdk ticket error")
